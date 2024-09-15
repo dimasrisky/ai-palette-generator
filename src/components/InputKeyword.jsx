d02b8d5fd07585fb15a00a490434b73db0cf4e73
dimasrisky <rizkyalphazee@gmail.com>
2024-09-16 00:18:14 +0700

    (form): mobile layout done

--- src/components/InputKeyword.jsx
+++ src/components/InputKeyword.jsx
@@ -10,9 +10,9 @@ export const InputKeyword = () => {
     window.location.reload()
   }
   return (
-  <div className='flex items-center gap-[12px]'>
-    <input type="text" className='w-[500px] pl-[28px] h-[55px] text-[18px] font-inter border border-[#DDDDDD] bg-[#F0F3F7] rounded-[10px] outline-none text-primary-gray transition-all duration-300 ease-in-out focus:ring-2 focus:ring-primary-blue focus:border-primary-blue focus:bg-white' placeholder='Type your keyword...' onChange={(event) => setInputKeyword(event.target.value)} value={inputKeyword}/>
-    <button className='bg-primary-black flex items-center gap-[8px] h-[55px] px-[30px] rounded-[10px] transition-all duration-300 ease-in-out hover:bg-opacity-80 hover:scale-105' onClick={submit}>
+  <div className='flex items-center gap-[12px] flex-col'>
+    <input type="text" className='w-[400px] pl-[28px] h-[55px] text-[18px] font-inter border border-[#DDDDDD] bg-[#F0F3F7] rounded-[10px] outline-none text-primary-gray transition-all duration-300 ease-in-out focus:ring-2 focus:ring-primary-blue focus:border-primary-blue focus:bg-white' placeholder='Type your keyword...' onChange={(event) => setInputKeyword(event.target.value)} value={inputKeyword}/>
+    <button className='bg-primary-black flex items-center justify-center gap-[8px] h-[55px] w-[400px] rounded-[10px] transition-all duration-300 ease-in-out hover:bg-opacity-80 hover:scale-105' onClick={submit}>
       <p className='text-white font-inter text-[16px]'>Generate</p>
       <img src={arrow} alt="arrow" width={'33px'}/>
     </button>
--- src/pages/Form.jsx
+++ src/pages/Form.jsx
@@ -1,17 +1,17 @@
 import stars from '../assets/icons/stars.svg'
 import { InputKeyword } from '../components/InputKeyword'
-
+// w-[976px]
 const Form = () => {
   return (
     <>
         <div className="w-[100vw] h-[100vh] flex justify-center items-center">
             <div className="flex flex-col items-center gap-[44px]">
-              <div className='flex flex-col items-center'>
+              <div className='flex flex-col items-center gap-[4px]'>
                 <div className='flex items-center gap-[12px]'>
-                  <img src={stars} alt="star"/>
-                  <h1 className="font-nexa-bold text-[48px]">Generate new palette</h1>
+                  <img src={stars} alt="star" width={'30px'}/>
+                  <h1 className="font-nexa-bold text-[30px]">Generate new palette</h1>
                 </div>
-                <h3 className='font-medium text-primary-gray text-[15px]'>Enter keyword for generating pallete (ex: soft,warm,cool)</h3>
+                <h3 className='font-medium text-primary-gray text-[8px]'>Enter keyword for generating pallete (ex: soft,warm,cool)</h3>
               </div>
               <InputKeyword />
             </div>
