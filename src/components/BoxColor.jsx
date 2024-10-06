b281314c3c5c27353e2846bb1810701d82087c25
dimasrisky <rizkyalphazee@gmail.com>
2024-10-06 12:10:39 +0700

    adding color name in box color mobile mode

--- src/components/BoxColor.jsx
+++ src/components/BoxColor.jsx
@@ -23,16 +23,19 @@ export default function BoxColor({ color, hex, index}){
             animate={{opacity: 1, scale: 1}}
             transition={{ease: 'easeInOut', delay: 0.1 * index, duration: 0.7}}
         >
-            <div className={`w-full h-[80px] sm:h-[340px] rounded-[25px] relative shadow-xl`} style={{ backgroundColor: hex }}>
-                <div className="w-full h-full flex  items-center justify-center relative group">
+            <div className={`w-full h-[80px] sm:h-[340px] rounded-[25px] relative shadow-xl`} style={{ backgroundColor: hex }} onClick={() => copyTextToClipboard(hex)}>
+                <div className="sm:hidden w-full h-full flex  items-center justify-center relative group">
                     {/* Hidden text that appears on hover */}
-                    <div className="absolute inset-0 gap-[8px] bg-[linear-gradient(rgba(255,255,255,0.5),rgba(255,255,255,0.2))] rounded-[25px] flex justify-center items-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
-                        <h1 className="text-black text-sm tracking-wide text-shadow-lg">
-                            {hex}
-                        </h1>
-                        <button onClick={() => copyTextToClipboard(hex)}>
-                            <img src={isCopied ? check : copy} alt="copy" width={'12px'}/>
-                        </button>
+                    <div className="absolute inset-0 bg-[linear-gradient(rgba(255,255,255,0.5),rgba(255,255,255,0.2))] rounded-[25px] flex justify-center items-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+                        <div className="flex flex-col gap-1 items-center font-semibold">
+                            <h1 className="text-[#333] text-xl tracking-wide text-shadow-lg">{color}</h1>
+                            <div className="flex items-center gap-[8px]">
+                                <h1 className="text-[#333] text-xs tracking-wide text-shadow-lg">{hex}</h1>
+                                <button onClick={() => copyTextToClipboard(hex)}>
+                                    <img src={isCopied ? check : copy} alt="copy" width={'12px'}/>
+                                </button>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
